f79643a6166e9cf43909ba340426b7406434e714
pradip2113 <21pradipjadhav@gmail.com>
2023-08-29 16:48:38 +0530

    29082023

--- cutomize/customize_app/doctype/ticket_track_history/ticket_track_history.js
+++ cutomize/customize_app/doctype/ticket_track_history/ticket_track_history.js
@@ -1,129 +1,71 @@
 // Copyright (c) 2023, Quantbit Team and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on('Ticket Track History', {
-
+    refresh: function(frm) {
+        $('.layout-side-section').hide();
+        $('.layout-main-section-wrapper').css('margin-left', '0');
+    }
 });
-// frappe.ui.form.on('Ticket Track History', {
-// 	show: function(frm) {
-// 		frm.call({
-// 			method:'get_data',//function name defined in python
-// 			doc: frm.doc, //current document
-// 		});
-// 	}
-// });
 
-// frappe.ui.form.on('Ticket Track History', {
-//     refresh: function(frm) {
-//         $('.layout-side-section').hide();
-//         $('.layout-main-section-wrapper').css('margin-left', '0');
-//          frm.add_custom_button(__('Get Report'), function() {
-//             // exportToExcel(frm);
-//             frappe.msgprint("Hi")
-//         });
-//         loadVersionData(frm);
-//         frm.page.clear_primary_action();
-//     },
-//     ticket_name: function(frm) {
-//         loadVersionData(frm);
-//         frm.page.clear_primary_action();
-//     },
-//     from_date: function(frm) {
-//         loadVersionData(frm);
-//         frm.page.clear_primary_action();
-//     },
-//     to_date: function(frm) {
-//         loadVersionData(frm);
-//         frm.page.clear_primary_action();
-//     }
-// });
+frappe.ui.form.on('Ticket Track History', {
+	refresh: function(frm) {
+		frm.clear_table("child_ticket")
+		frm.refresh_field('child_ticket')
+		frm.call({
+			method:'get_ticket_list',//function name defined in python
+			doc: frm.doc, //current document
+		});
 
-// function loadVersionData(frm) {
-//     var page_length = 200;
-//     var page_number = 1;
-//     var from_date = frm.doc.from_date || '';
-//     var to_date = frm.doc.to_date || '';
-//     var ticket_name = frm.doc.ticket_name || '';
+	}
+});
 
-//     var filters = {
-//         'docname': ['like', '%' + ticket_name + '%'],
-//         'ref_doctype': 'Issue',
-//     };
-//     if (from_date && to_date) {
-//         filters['modified'] = ['between', [from_date + ' 00:00:00', to_date + ' 23:59:59']];
-//     } else if (ticket_name) {
-//         filters['docname'] = ['like', '%' + ticket_name + '%'];
-//     }
+frappe.ui.form.on('Ticket Track History', {
+	onload: function(frm) {
+		frm.call({
+			method:'set_default_date',//function name defined in python
+			doc: frm.doc, //current document
+		});
 
-//     frappe.call({
-//         method: 'frappe.client.get_list',
-//         args: {
-//             doctype: 'Version',
-//             fields: ['docname', 'data', 'modified', 'modified_by', 'ref_doctype'],
-//             limit_start: (page_number - 1) * page_length,
-//             limit_page_length: page_length,
-//             filters: filters,
-//         },
-//         callback: function(response) {
-//             var data = response.message;
-//             var help_content = `
-//                 <div class="table-responsive">
-//                     <table class="table table-bordered">
-//                         <thead>
-//                             <tr>
-//                                 <th style="position: sticky; top: 0; background-color: white; width:10%">Ticket Name</th>
-//                                 <th style="position: sticky; top: 0; background-color: white;width:10%">Date</th>
-//                                 <th style="position: sticky; top: 0; background-color: white;">Field</th>
-//                                 <th style="position: sticky; top: 0; background-color: white;">Old data</th>
-//                                 <th style="position: sticky; top: 0; background-color: white;">New data</th>
-//                                 <th style="position: sticky; top: 0; background-color: white;">User</th>
-//                             </tr>
-//                         </thead>
-//                         <tbody>
-//             `;
+	}
+});
+frappe.ui.form.on('Ticket Track History', {
+	ticket_name: function(frm) {
+		frm.clear_table("child_ticket")
+		frm.refresh_field('child_ticket')
+		frm.refresh_field('custom_serial_number')
+		frm.call({
+			method:'get_ticket_list',//function name defined in python
+			doc: frm.doc, //current document
+		});
 
-//             for (var i = 0; i < data.length; i++) {
-//                 var docname = data[i].docname || '';
-//                 var modified = data[i].modified || '';
-//                 var userValue = data[i].modified_by || '';
-//                 var changesData = JSON.parse(data[i].data);
-//                 var changes = changesData.changed || [];
+	}
+});
 
-//                 for (var j = 0; j < changes.length; j++) {
-//                     var field = changes[j][0];
-//                     var oldValue = changes[j][1];
-//                     var newValue = changes[j][2];
-//                     if (field == "signature") {
-//                         if (oldValue) {
-//                             oldValue = "True"
-//                         }
+frappe.ui.form.on('Ticket Track History', {
+	custom_serial_number: function(frm) {
+		frm.clear_table("child_ticket")
+		frm.refresh_field('child_ticket')
+		frm.refresh_field('ticket_name')
+		frm.call({
+			method:'get_ticket_list',//function name defined in python
+			doc: frm.doc, //current document
+		});
 
-//                         if (newValue) {
-//                             newValue = "True"
-//                         }
-//                     }
-//                     help_content += `
-//                         <tr>
-//                             <td>${j === 0 ? docname : ''}</td>
-//                             <td>${j === 0 ? modified : ''}</td>
-//                             <td>${field}</td>
-//                             <td>${oldValue}</td>
-//                             <td>${newValue}</td>
-//                             <td>${j === 0 ? userValue : ''}</td>
-//                         </tr>
-//                     `;
-//                 }
-//             }
+	}
+});
 
-//             help_content += `
-//                         </tbody>
-//                     </table>
-//                 </div>
-//             `;
+frappe.ui.form.on('Ticket Track History', {
+	to_date: function(frm) {
+		frm.clear_table("child_ticket")
+		frm.refresh_field('child_ticket')
+		frm.call({
+			method:'get_ticket_list',//function name defined in python
+			doc: frm.doc, //current document
+		});
 
-//             // Insert HTML content into the specified field
-//             frm.fields_dict["show_html"].$wrapper.html(help_content);
-//         }
-//     });
-// }
+	}
+});
+
 
--- cutomize/customize_app/doctype/ticket_track_history/ticket_track_history.py
+++ cutomize/customize_app/doctype/ticket_track_history/ticket_track_history.py
@@ -1,10 +1,86 @@
 # Copyright (c) 2023, Quantbit Team and contributors
 # For license information, please see license.txt
 
+import json
+import os
 import frappe
 from frappe.model.document import Document
+from datetime import datetime, timedelta
 
 class TicketTrackHistory(Document):
-	pass
-	# def get_data(self):
-	# 	frappe.msgprint("hi")
+	@frappe.whitelist()
+	def get_ticket_list(self):
+		filters = {"ref_doctype": 'Issue'}
+		if self.from_date and self.to_date:
+			filters["modified"] = ["between", [self.from_date, self.to_date]]
+		if self.ticket_name:
+			filters["docname"] = self.ticket_name
+		
+		data = frappe.get_list("Version",
+							filters=filters,
+							fields=['docname', 'modified', 'modified_by', 'data'])
+		
+		for item in data:
+			docname = item.docname or ''
+			modified = item.modified or ''
+			user_value = item.modified_by or ''
+			data_dict = json.loads(item.data)
+			changes = data_dict.get("changed", [])
+			for change in changes:
+				field, old_value, new_value = change[0], change[1], change[2]
+				if old_value is None:
+					old_value = 0
+				if new_value is None:
+					new_value = 0
+				if field == 'description':
+					old_value = "Yes" if old_value else ""
+					new_value = "Yes" if new_value else ""
+				if field == "signature":
+					old_value = "Yes" if old_value else ""
+					new_value = "Yes" if new_value else ""
+
+				issue_serial = frappe.get_value("Issue", filters={"name": docname}, fieldname='serial_number') or ''
+				if(self.custom_serial_number):
+					if(self.custom_serial_number==issue_serial):
+						self.append(
+								"child_ticket",	
+								{
+									'ticket_name':docname,
+									'serial_no':issue_serial,
+									'date_time':str(modified),
+									'field':str(field),
+									'previous_value':str(old_value),
+									'modified_value':str(new_value),
+									'modified_user_email': str(user_value),
+										
+								}
+						)
+				else:
+					self.append(
+							"child_ticket",	
+							{
+								'ticket_name':docname,
+								'serial_no':issue_serial,
+								'date_time':str(modified),
+								'field':str(field),
+								'previous_value':str(old_value),
+								'modified_value':str(new_value),
+								'modified_user_email': str(user_value),
+									
+							}
+					)
+					
+	@frappe.whitelist()
+	def set_default_date(self):
+		current_year = datetime.now().year
+		current_month = datetime.now().month
+		default_from_date, default_to_date =self.generate_default_date_range(current_year, current_month)
+		self.from_date = default_from_date.strftime("%Y-%m-%d %H:%M:%S")
+		self.to_date = default_to_date.strftime("%Y-%m-%d %H:%M:%S")
+
+	def generate_default_date_range(self,year,month):
+		default_from_date = datetime(year, month, 1)
+		default_to_date = default_from_date.replace(day=28) + timedelta(days=3)
+		if default_to_date.month != month:
+			default_to_date = default_from_date.replace(day=28) + timedelta(days=2)
+		return default_from_date, default_to_date
